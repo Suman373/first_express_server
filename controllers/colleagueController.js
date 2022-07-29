3a6f4a0630398c380f2e989c00c6b81000d0c15b
Suman <iamsuman898@gmail.com>
2022-07-30 00:11:16 +0530

    add:controllers complete

--- controllers/colleagueController.js
+++ controllers/colleagueController.js
@@ -9,13 +9,21 @@ const data = {
 
 // get one specific colleague ,get
 const getColleague = (req,res)=>{
-    res.json({ "id" : req.params.id }); // pulling in the parameter directly from the url , when a certain id is routed
+   // create colleague check if exists
+   const colleague = data.colleagues.find(coll=> coll.id !== parseInt(req.params.id)); // as we pull it out of the url /:id
+   // if colleague not found
+   if(!colleague){
+    // error
+        return res.status(400).json({"message": `Colleague with ID ${req.params.id}`})
+   }
+    res.json(data.colleagues); // send the data
 }
 
 // get all colleague request, read
 const getAllColleagues =(req,res)=>{
     res.json(data.colleagues); // we get all the colleagues by referencing data.colleagues
 }
+
 
 // post request, create colleague
 const createNewColleague = (req,res)=>{
@@ -36,17 +44,43 @@ const createNewColleague = (req,res)=>{
    res.status(201).json(data.colleagues); // 201 : record created
 }
 
+
 // put request , update one specific
 const updateColleague = (req,res)=>{
-    res.json({
-        "name" : req.body.name,
-        "position": req.body.position
-    });
+    // create colleague 
+    const colleague = data.colleague.find(coll=> coll.id === parseInt(req.body.id)); //find the user in valid id
+    if(!colleague){// colleague not found
+        return res.status(400).json({ "message" : `Colleague ID ${req.body.id} not found!` });// error
+    } 
+    // if exists
+    if(req.body.name) colleague.name = req.body.name; // set the new name
+    if(req.body.position) colleague.position = req.body.position; // set the new position
+    // filter array where existing record of colleague is removed
+    const filteredArray = data.colleagues.filter(coll => coll.id !== parseInt(req.body.id));
+    // non-chronological array where the new colleague is added to the filtered array
+    const unsortedArray = [...filteredArray,colleague]; // add the colleague
+    // sort the array 
+    data.setColleagues(unsortedArray.sort((a,b)=> a.id > b.id ? 1 : a.id < b.id ? -1 : 0 ));
+    res.json(data.colleagues);
 }
+
+
 
 // del request, delete 
 const deleteColleague =(req,res)=>{
-    res.json({ "id": req.body.id});
+     // create colleague 
+     const colleague = data.colleague.find(coll=> coll.id === parseInt(req.body.id)); //find the user in valid id
+     if(!colleague){// colleague not found
+         return res.status(400).json({ "message" : `Colleague ID ${req.body.id} not found!` });// error
+     } 
+     // if exists
+     if(req.body.name) colleague.name = req.body.name; // set the new name
+     if(req.body.position) colleague.position = req.body.position; // set the new position
+     // filter array where existing record of colleague is removed
+     const filteredArray = data.colleagues.filter(coll => coll.id !== parseInt(req.body.id));
+    //  now, set the filtered array to the setColleagues
+     data.setColleagues([...filteredArray]);
+     res.json(data.colleagues); // return colleagues
 }
 
 module.exports = { getColleague, getAllColleagues , createNewColleague , updateColleague, deleteColleague};
