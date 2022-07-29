63e2e316ae17acc1c72449ee7fcc005c0dc71a6f
Suman <iamsuman898@gmail.com>
2022-07-29 22:45:47 +0530

    add: model+controllers

--- controllers/colleagueController.js
+++ controllers/colleagueController.js
@@ -1,6 +1,10 @@
 // controls the logic for route handling 
-const data ={};
-data.colleagues = require('../../data/colleagues.json'); // like connecting to db
+const data = {
+    // similar to react --> considering MERN
+    colleagues : require('../model/colleagues.json'),// like connecting to db
+    setColleagues : function (data) {this.colleagues = data;}
+};
+
 
 
 // get one specific colleague ,get
@@ -10,16 +14,26 @@ const getColleague = (req,res)=>{
 
 // get all colleague request, read
 const getAllColleagues =(req,res)=>{
-    res.json(data.colleagues);
+    res.json(data.colleagues); // we get all the colleagues by referencing data.colleagues
 }
 
 // post request, create colleague
 const createNewColleague = (req,res)=>{
-    res.json({
-        // getting parameters from post request and sending them back
-        "name" : req.body.name,
-        "position": req.body.position
-    });
+   const newColleague = {
+    //  we create new colleague by +1 to the last id , if no user then 1
+     id: data.colleagues[data.colleagues.length-1].id + 1 || 1,
+     // store name and position
+     name: req.body.name,
+     position: req.body.position
+   }
+   // checks for null values
+   if( !newColleague.name || !newColleague.position){
+       // handle error
+       return res.status(400).json({'message': 'Both name and position required.'});
+   }
+
+   data.setColleagues([...data.colleagues, newColleague]); // copy all others , then add new one after them
+   res.status(201).json(data.colleagues); // 201 : record created
 }
 
 // put request , update one specific
